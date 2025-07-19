ecdccaac23df72ae3ab1d61c08b34780b8aad47f
Siddhantbg <siddhant.bhagat004@gmail.com>
2025-07-20 00:59:25 +0530

    commit

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,4 @@
-const production = true;
+const production = process.env.NODE_ENV === 'production';
 
 export const serverUrl = production
   ? 'https://noise-signal.vercel.app' // Replace with your deployed server URL
