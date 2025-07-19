829072f9a3b71ac771fdbc8680cf6a864c4ff89c
Siddhantbg <siddhant.bhagat004@gmail.com>
2025-07-19 17:39:58 +0530

    deployed

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,5 +1,5 @@
 const production = true;
 
 export const serverUrl = production
-  ? 'https://your-deployed-server-url.com' // Replace with your deployed server URL
+  ? 'https://noise-signal.vercel.app/' // Replace with your deployed server URL
   : 'http://localhost:5000';
