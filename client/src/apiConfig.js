03fd02cf8fee638a4a674f51fd8a774b10debff2
Siddhantbg <siddhant.bhagat004@gmail.com>
2025-07-20 02:02:57 +0530

    Update apiConfig.js

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,5 +1,5 @@
 const production = process.env.NODE_ENV === 'production';
 
 export const serverUrl = production
-  ? 'https://noise-signal.vercel.app' // Replace with your deployed server URL
+  ? 'https://noise-signal-server.onrender.com' // Replace with your deployed server URL
   : 'http://localhost:5000';
